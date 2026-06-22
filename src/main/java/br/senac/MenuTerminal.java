b02da780ebf7af45342c52f43392b3a3be657101
Lucas Neves <lucasneves4004@gmail.com>
2026-06-22 18:00:22 -0300

    add test

--- src/main/java/br/senac/MenuTerminal.java
+++ src/main/java/br/senac/MenuTerminal.java
@@ -13,12 +13,16 @@ import java.util.List;
 import java.util.Scanner;
 
 @Component
+// comandline parecdio com um main para rodar o codigo
 public class MenuTerminal implements CommandLineRunner {
 
+
+    
     private final ClientesService clientesService;
     private final MercadoriasService mercadoriasService;
     private final ServicoService servicoService;
 
+    // faz um construtor de todas as clases 
     public MenuTerminal(ClientesService clientesService, 
                         MercadoriasService mercadoriasService, 
                         ServicoService servicoService) {
@@ -27,15 +31,16 @@ public class MenuTerminal implements CommandLineRunner {
         this.servicoService = servicoService;
     }
 
+
+        // aqui ele pega os dados do service e faz rodar tipo o mais que seria o run
+        
     @Override
     public void run(String... args) {
         Scanner scanner = new Scanner(System.in);
         String opcao = "";
 
-        while (!opcao.equals("0")) {
-            System.out.println("\n==========================================");
+        while (!opcao.equals("0")) { // so sai do while se for = 0
             System.out.println("      SENAC SOLUTIONS - PAINEL ADMIN      ");
-            System.out.println("==========================================");
             System.out.println("1 - Modulo de Clientes");
             System.out.println("2 - Modulo de Equipamentos");
             System.out.println("3 - Modulo de Ordens de Servico (OS)");
@@ -45,6 +50,8 @@ public class MenuTerminal implements CommandLineRunner {
 
             opcao = scanner.nextLine();
 
+
+            // aqui entra nos outros menus
             switch (opcao) {
                 case "1": menuClientes(scanner); break;
                 case "2": menuEquipamentos(scanner); break;
@@ -65,20 +72,27 @@ public class MenuTerminal implements CommandLineRunner {
         String op = scanner.nextLine();
 
         if (op.equals("1")) {
+            // aqui criar um objt vazio
+
             Clientes cliente = new Clientes();
+
             System.out.print("Nome: "); cliente.setNome(scanner.nextLine());
             System.out.print("CPF: "); cliente.setCpf(scanner.nextLine());
-            
-            // Tentei usar get/set Telefone e Email. Se sua entidade Clientes não tiver, apague as 2 linhas abaixo
             System.out.print("Telefone: "); cliente.setTelefone(scanner.nextLine());
             System.out.print("E-mail: "); cliente.setEmail(scanner.nextLine()); 
             
+            // aqui faz o insert
             clientesService.salvar(cliente);
             System.out.println("=> Cliente cadastrado com sucesso!");
 
         } else if (op.equals("2")) {
+
             System.out.println("\n--- LISTA DE CLIENTES ---");
-            List<Clientes> lista = clientesService.listarTodos();
+
+            List<Clientes> lista = clientesService.listarTodos(); // aqui faz o select * from clientes
+
+            // aqui printa na tela os cliente
+
             for (Clientes c : lista) {
                 System.out.println("ID: " + c.getId() + " | Nome: " + c.getNome() + " | CPF: " + c.getCpf());
             }
@@ -114,7 +128,7 @@ public class MenuTerminal implements CommandLineRunner {
         }
     }
 
-    // --- MÓDULO DE ORDENS DE SERVIÇO ---
+    
     private void menuOS(Scanner scanner) {
         System.out.println("\n--- CONTROLE DE OS ---");
         System.out.println("1 - Abrir Nova OS");
@@ -122,18 +136,23 @@ public class MenuTerminal implements CommandLineRunner {
         System.out.print("Escolha: ");
         String op = scanner.nextLine();
 
+
         if (op.equals("1")) {
+
             Servico servico = new Servico();
             
             
-            System.out.print("Digite o ID do Cliente (ja cadastrado): ");
-            Long idCliente = Long.parseLong(scanner.nextLine());
+            System.out.print("Digite o ID do Cliente: ");
+
+            Long idCliente = Long.parseLong(scanner.nextLine()); // aqui pega o id cliente e convert para long para conseguir fazer o select
+
             Clientes cliente = new Clientes();
+
             cliente.setId(idCliente);
             servico.setCliente(cliente); 
             
             
-            System.out.print("Digite o ID do Equipamento (ja cadastrado): ");
+            System.out.print("Digite o ID do Equipamento: ");
             Long idMerc = Long.parseLong(scanner.nextLine());
             Mercadorias mercadoria = new Mercadorias();
             mercadoria.setIdmerc(idMerc);
@@ -159,10 +178,53 @@ public class MenuTerminal implements CommandLineRunner {
 
         } else if (op.equals("2")) {
             System.out.println("\n--- LISTA DE OS ---");
+
             List<Servico> lista = servicoService.ListarTodosServicos();
+
             for (Servico s : lista) {
                 
                 System.out.println("ID: " + s.getId() + " | Cliente: " + s.getCliente().getNome() + " | Status: " + s.getStatus());
+            }
+        }
+        else if (op.equals("3")) {
+
+            System.out.println("\nATUALIZAR STATUS DA OS");
+
+            System.out.print("Digite o ID da OS que deseja atualizar: ");
+            
+            Long idOs = Long.parseLong(scanner.nextLine());
+
+            Servico osEncontrada = null;
+
+            for (Servico s : servicoService.ListarTodosServicos()) {
+                if (s.getId().equals(idOs)) {
+                    osEncontrada = s; // Achou a OS!
+                    break;
+                }
+            }
+
+            if (osEncontrada != null) {
+
+                System.out.println("Status atual: " + osEncontrada.getStatus());
+                System.out.println("Novo Status:");
+                System.out.println("1 - Em Andamento");
+                System.out.println("2 - Aguardando Peça");
+                System.out.println("3 - Finalizada (Encerrar)");
+                System.out.print("Escolha: ");
+                String novoStatus = scanner.nextLine();
+
+                if (novoStatus.equals("1")) osEncontrada.setStatus("Em Andamento");
+                else if (novoStatus.equals("2")) osEncontrada.setStatus("Aguardando Peca");
+                else if (novoStatus.equals("3")) osEncontrada.setStatus("Finalizada");
+
+                
+                servicoService.SalvarServico(osEncontrada);
+
+                System.out.println("Status da OS atualizado com sucesso!");
+
+            } else {
+
+                System.out.println("OS nao encontrada no banco de dados.");
             }
         }
     }
@@ -178,17 +240,21 @@ public class MenuTerminal implements CommandLineRunner {
 
         switch (op) {
             case "1":
-                System.out.println("\n[RELATORIO DE CLIENTES]");
+                System.out.println("\nRELATORIO DE CLIENTES CADASTRADO");
+
                 clientesService.listarTodos().forEach(c -> System.out.println("- " + c.getNome() + " (CPF: " + c.getCpf() + ")"));
+
                 break;
             case "2":
-                System.out.println("\n[RELATORIO DE EQUIPAMENTOS]");
+                System.out.println("\nRELATORIO DE EQUIPAMENTOS");
                 mercadoriasService.ListarTodos().forEach(m -> System.out.println("- " + m.getMarca() + " " + m.getModelo() + " | Defeito: " + m.getDescDefeito()));
+
                 break;
             case "3":
-                System.out.println("\n[RELATORIO DE ORDENS DE SERVICO]");
-                // Atualizado para usar o .getCliente().getNome()
+                System.out.println("\nRELATORIO DE ORDENS DE SERVICO");
+
                 servicoService.ListarTodosServicos().forEach(s -> System.out.println("- OS " + s.getId() + " | Cliente: " + s.getCliente().getNome() + " | Status: " + s.getStatus()));
+
                 break;
         }
     }
